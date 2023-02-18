2ce0ddf9fb9372bfcab95597dcae282c250bff37
myUsername <myemail.com>
2023-02-17 16:50:04 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,15 +4,7 @@
 
 /**
  * main - prints all possible different combinations of two digits.
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -42,5 +34,5 @@ int main(void)
 		c++;
 	}
 	putchar('\n');
-	return (0);
+	return (0
 }
