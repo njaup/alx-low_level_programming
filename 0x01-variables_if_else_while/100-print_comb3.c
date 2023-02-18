c371b503e35c8501e8aaad7c55df5f3e119a5bf3
myUsername <myemail.com>
2023-02-17 17:21:58 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,30 +9,27 @@
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int ones = '0';
+	int tens = '0';
 
-	while (d < 10)
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digitds*/
 	{
-		c = 0;
-		while (c < 10)
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digits*/
 		{
-			if (d != c && d < c)
+			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 			{
-				putchar('0' + d);
-				putchar('0' + c);
+				putchar(tens);
+				putchar(ones);
 
-				if (c + d != 17)
+				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
 
-			c++;
 		}
-		c++;
 	}
 	putchar('\n');
-	return (0
+	return (0);
 }
