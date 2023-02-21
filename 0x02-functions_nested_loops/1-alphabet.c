8b55803107006ab3445c58bfc7100aebec1dcb0f
myUsername <myemail.com>
2023-02-21 12:43:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
