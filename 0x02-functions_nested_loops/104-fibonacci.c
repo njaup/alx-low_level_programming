73c0960859fd0bae0c0f7165e0c4c94e90bb45ed
myUsername <myemail.com>
2023-02-21 19:26:19 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,6 +5,7 @@
  * 1 and 2, separated by a comma followed by  a space
  * Return: Always 0
  */
+
 int main(void)
 {
 	int count;
@@ -12,7 +13,7 @@ int main(void)
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (count = 0; count <92; count++)
+	for (count = 0; count < 92; count++)
 	{
 	sum = fib1 + fib2;
 	printf("%lu, ", sum);
