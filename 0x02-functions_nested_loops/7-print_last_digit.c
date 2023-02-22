d667b65b2de693c8573142d27b9892837bd84c58
myUsername <myemail.com>
2023-02-21 16:00:03 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,8 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: The int to extract the last digit 
- * Return: value of the last digit 
+ * @n: The int to extract the last digit
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
