d7e807eb5b4503288fd6c9634245af84f9325443
myUsername <myemail.com>
2023-02-27 17:23:11 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _atoi - convert a strinig to an integer
  * @s: string to convert
  * Return: an integer
  */
+
 int _atoi(char *s)
 {
 	int i = 0;
@@ -12,14 +14,15 @@ int _atoi(char *s)
 
 	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 	{
-	if (s[i] == '_')
+	if (s[i] == '-')
 		signo *= -1;
+	i++;
 	}
 	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 	{
 	if (n >= 0)
 	{
-	n = n * 10 + (s[i] - '0');
+	n = n * 10 - (s[i] - '0');
 	i++;
 	}
 	else
