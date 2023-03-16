efb1cc6fa8bbc0e526474cbf13effef73276398e
myUsername <myemail.com>
2023-03-16 15:39:24 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocates memory for an array, using malloc
+ * @nmemb: element in the array
+ * @size: bytes
+ * Return: If nmemb or size is 0, then _calloc returns NULL
+ * If malloc fails, then _calloc returns NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
