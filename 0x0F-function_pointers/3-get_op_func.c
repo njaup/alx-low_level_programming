f8596c024716d1b1a3454356e473f23eaffdff44
myUsername <myemail.com>
2023-03-23 11:56:02 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - selects the correct fuction
+ * to perform the operation asked by the user
+ * @s: the operator passed as an argument
+ * Return: If s does not match any of the 5 expected
+ * operators (+, -, *, /, %), return NULL
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ * op_add - returns the sum of the two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference of the two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product of the two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the division of the two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the modulus of the two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
