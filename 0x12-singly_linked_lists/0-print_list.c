d8cc59adfb652e729a9d25cd5e8a6a91cec408f3
njaup <njaup974@gmail.com>
2023-03-30 18:37:17 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-
+	size_t nodes;
+		
+	nodes = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
 		h = h->next;
+		nodes++;
 	}
 
 	return (nodes);
