c038265a1f9d9e632c93a9c9faccea1600ef8cb3
njaup <njaup974@gmail.com>
2023-04-05 11:53:29 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - prints the minimum number of coins
@@ -10,34 +11,36 @@
  */
 int main(int argc, char *argv[])
 {
-	int total, aux, position, change;
+	int sum, aux, point, change;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	total = aux = position = change = 0;
+	sum = aux = point = change = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	total = atoi(argv[1]);
+	sum = atoi(argv[1]);
 
-			if (total <= 0)
+			if (sum <= 0)
 			{
-			printf("0\n");
+			printf("0");
+			printf("\n");
 			return (0);
 			}
-			while (coins[position] != '\0')
+			while (coins[point] != '\0')
 
 			{
-			if (total >= coins[position])
+			if (sum >= coins[point])
 			{
-			aux = (total / coins[position]);
+			aux = (sum / coins[point]);
 			change += aux;
-			total -= coins[position] * aux;
+			sum -= coins[point] * aux;
 			}
-			position++;
+			point++;
 			}
 
-			printf("%d\n", change);
+			printf("%d", change);
+			printf("\n");
 			return (0);
 }
