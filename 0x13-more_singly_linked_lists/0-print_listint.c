5a9a20b78bec4401093eed56000b37aef40d0791
njaup <njaup974@gmail.com>
2023-04-04 18:15:47 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *current;
+	const listint_t *current = h;
 	size_t count = 0;
 
 	for (current = h; current != NULL; current = current->next)
 	{
-		printf("%d\n", current->n);
 		count++;
+		printf("%d\n", current->n);
 	}
 
 	return (count);
