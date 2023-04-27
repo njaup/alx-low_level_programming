24ee3014a9cee4ca63b11016b1dbb2f41ad1a5b5
njaup <njaup974@gmail.com>
2023-04-27 15:29:35 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,12 +24,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
 	if (fd == -1)
 	{
 		return (-1);
 	}
-
 	if (text_content != NULL)
 	{
 		ssize_t len = strlen(text_content);
@@ -41,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	
+
 	close(fd);
 	return (1);
 }
