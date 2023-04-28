4c34e99341dd54892bee5cebe26da4ed9b914a6a
njaup <njaup974@gmail.com>
2023-04-28 07:30:50 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,44 +24,44 @@ void print_error(int error_code, const char *message,
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fd_f, fd_t, b_read, b_written;
 	char buffer[BUFFER_SIZE];
 
-	if (argc != 3)
+	if (argc == 3)
 	{
 		dprintf(STDERR_FILENO, "usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	fd_f = open(argv[1], O_RDONLY);
+	if (fd_f == -1)
 	{
-		print_error(98, "Error: cant read from file %s\n", argv[2], fd_from);
+		print_error(98, "Error: cant read from file %s\n", argv[2], fd_f);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+	fd_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to == -1)
+	if (fd_t == -1)
 	{
-		print_error(99, "Error: cant write to %s\n", argv[2], fd_from);
+		print_error(99, "Error: cant write to %s\n", argv[2], fd_f);
 	}
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((b_read = read(fd_f, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		b_written = write(fd_t, buffer, b_read);
+		if (b_written == b_read)
 		{
-			print_error(99, "Error: cant write to %s\n", argv[2], fd_from);
+			print_error(99, "Error: cant write to %s\n", argv[2], fd_f);
 		}
 	}
-	if (bytes_read == -1)
+	if (b_read == -1)
 	{
-		print_error(98, "Error: cant read from file %s\n", argv[1], fd_to);
+		print_error(98, "Error: cant read from file %s\n", argv[1], fd_t);
 	}
-	if (close(fd_from) == -1)
+	if (close(fd_f) == -1)
 	{
-		print_error(100, "Error: cant close fd %d\n", argv[1], fd_to);
+		print_error(100, "Error: cant close fd %d\n", argv[1], fd_t);
 	}
-	if (close(fd_to) == -1)
+	if (close(fd_t) == -1)
 	{
-		print_error(100, "Error: cant close fd %d\n", argv[2], fd_from);
+		print_error(100, "Error: cant close fd %d\n", argv[2], fd_f);
 	}
 	return (0);
 }
