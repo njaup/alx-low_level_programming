308d5069d07f366b86ceba9ac6baf1accf01a59b
njaup <njaup974@gmail.com>
2023-04-28 07:17:48 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,8 @@
 
 #define BUFFER_SIZE 1024
 
-void print_error(int error_code, const char *message, const char *file_name, int fd)
+void print_error(int error_code, const char *message,
+		const char *file_name, int fd)
 {
 	dprintf(STDERR_FILENO, message, file_name);
 	if (fd != -1)
@@ -15,7 +16,6 @@ void print_error(int error_code, const char *message, const char *file_name, int
 	}
 	exit(error_code);
 }
-
 /**
  * main - copies the content of a file to another file
  * @argc: arguments
@@ -32,19 +32,17 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		print_error(98, "Error: cant read from file %s\n", argv[2], fd_from);
 	}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
 		print_error(99, "Error: cant write to %s\n", argv[2], fd_from);
 	}
-
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -53,21 +51,17 @@ int main(int argc, char *argv[])
 			print_error(99, "Error: cant write to %s\n", argv[2], fd_from);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		print_error(98, "Error: cant read from file %s\n", argv[1], fd_to);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		print_error(100, "Error: cant close fd %d\n", argv[1], fd_to);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		print_error(100, "Error: cant close fd %d\n", argv[2], fd_from);
 	}
-
 	return (0);
 }
