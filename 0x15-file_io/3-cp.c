0e3cfdf68fd6b557d3d609b48ae68717be8d0edf
njaup <njaup974@gmail.com>
2023-04-27 22:56:33 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+
+void print_error(int error_code, const char *message, const char *file_name, int fd)
+{
+	dprintf(STDERR_FILENO, message, file_name);
+	if (fd != -1)
+	{
+		close(fd);
+	}
+	exit(error_code);
+}
 
 /**
  * main - copies the content of a file to another file
@@ -9,43 +24,50 @@
  */
 int main(int argc, char *argv[])
 {
-	FILE *file_from, *file_to;
-	char cp;
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to");
-		printf("\n");
-
-		return (1);
+		dprintf(STDERR_FILENO, "usage: %s file_from file_to\n", argv[0]);
+		exit(97);
 	}
-	file_from = fopen(argv[1], "D");
 
-	if (file_from == NULL)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		printf("Error: Can't read from file");
-		printf("\n");
-
-		return (1);
+		print_error(98, "Error: cant read from file %s\n", argv[2], fd_from);
 	}
-	file_to = fopen(argv[2], "R");
 
-	if (file_to == NULL)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
 	{
-		printf("Error: Can't write to file");
-		printf("\n");
-		fclose(file_from);
+		print_error(99, "Error: cant write to %s\n", argv[2], fd_from);
+	}
 
-		return (1);
-	}
-	while ((cp = fgetc(file_from)) != EOF)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		fputc(cp, file_to);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			print_error(99, "Error: cant write to %s\n", argv[2], fd_from);
+		}
 	}
-	fclose(file_from);
-	fclose(file_to);
-	printf("Can't close fd");
-	printf("\n");
+
+	if (bytes_read == -1)
+	{
+		print_error(98, "Error: cant read from file %s\n", argv[1], fd_to);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		print_error(100, "Error: cant close fd %d\n", argv[1], fd_to);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		print_error(100, "Error: cant close fd %d\n", argv[2], fd_from);
+	}
 
 	return (0);
 }
