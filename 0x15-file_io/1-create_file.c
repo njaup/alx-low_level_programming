6a9866d1281363908a389726d893a80d6925f8e5
njaup <njaup974@gmail.com>
2023-05-12 16:51:21 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,7 @@ int create_file(const char *filename, char *text_content)
 {
 	size_t length = strlen(text_content);
 	int file_d;
+	ssize_t num_w;
 
 	if (filename == NULL)
 	{
@@ -28,8 +29,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		ssize_t num_w = write(file_d, text_content, length);
-
+		num_w = write(file_d, text_content, length);
 		if (num_w == -1)
 		{
 			close(file_d);
